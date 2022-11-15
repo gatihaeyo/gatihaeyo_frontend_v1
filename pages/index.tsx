c0df0fe3d952464257cb83dfea0d1ac5ac978034
smp6340 <smp6340@naver.com>
2022-11-15 16:24:33 +0900

    🎨 :: First Paragraph 파일 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 // Pages
+import FirstPage from "./main/first";
 import SecondPage from "./main/second";
 import ThirdPage from "./main/third";
 
@@ -13,31 +14,13 @@ import Footer from "./common/func/footer";
 // Styles
 import * as S from "./style";
 
-// Images
-import { JinxImg } from "./common/image";
-
 const Home: NextPage = () => {
   return (
     <>
       <S.Layout>
         {/*<Loading />*/}
         <Header />
-        <S.Page>
-          <S.TitleWrapper>
-            <S.Title1>게임 같이할 </S.Title1>
-            <S.Title2>친구 </S.Title2>
-            <S.Title3>필요한가요?</S.Title3>
-          </S.TitleWrapper>
-          <S.ImageWrapper>
-            <S.ImageSubWrapper>
-              <Image
-                src={JinxImg}
-              />
-            </S.ImageSubWrapper>
-          </S.ImageWrapper>
-        </S.Page>
-        <S.LoginText>로그인 후 이용해보세요!</S.LoginText>
-        <S.LoginBtn>로그인</S.LoginBtn>
+        <FirstPage />
         <S.Contour />
         <SecondPage />
         <S.Contour />
--- pages/style.ts
+++ pages/style.ts
@@ -6,88 +6,8 @@ export const Layout = styled.div`
     height: 100vh;
 `;
 
-// Image Style
-export const ImageWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`;
-
-export const ImageSubWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    width: 620px;
-    height: 491px;
-    z-index: -1;
-`;
-
-// Title Style
-export const Page = styled.div`
-    -webkit-user-select:none;
-    -moz-user-select:none;
-    -ms-user-select:none;
-    user-select:none;
-    margin-top: 23.31px;
-    position: relative ;
-`;
-
-export const TitleWrapper = styled.div`
-    position: absolute;
-    margin-top: 200px;
-    margin-left: 300px;
-`;
-
-export const Title1 = styled.span`
-    font-family: 'NotoSansKR-Black';
-    font-weight: bold;
-    font-style: blod;
-    font-size: 96px;
-`;
-
-export const Title2 = styled.span`
-    font-family: 'NotoSansKR-Black';
-    font-weight: bold;
-    font-style: blod;
-    font-size: 96px;
-    color: #FFE75F;
-`;
-
-export const Title3 = styled.span`
-    font-family: 'NotoSansKR-Black';
-    font-weight: bold;
-    font-style: blod;
-    font-size: 96px;
-`;
-
-// About Login
-export const LoginText = styled.div`
-    position: absolute;
-    font: 700 normal 32px ${(props) => props.theme.fonts.kr};
-    color: ${(props) => props.theme.colors.Extra.black};
-    right: 656.11px;
-    top: 539.09px;
-`;
-
-export const LoginBtn = styled.button`
-    position: absolute;
-    font: 700 normal 32px ${(props) => props.theme.fonts.kr};
-    color: ${(props) => props.theme.colors.Extra.black};
-    background-color: #FFE75F;
-    color: #ffffff;
-    width: 200px;
-    height: 60px;
-    border-radius: 20px;
-    border: none;
-    right: 406.43px;
-    top: 532.99px;
-    :hover{
-        cursor: pointer;
-    }
-    box-shadow: -3px 3px 3px grey;
-`;
-
 export const Contour = styled.hr`
     width: 1720px;
     border: 3px solid #D9D9D9;
     margin-top: 68.21px;
 `;
-
