6fbf866e3e51f7c5d83e63ea0aed9d1ff4f8835a
smp6340 <smp6340@naver.com>
2022-11-17 17:31:45 +0900

    🎨 :: Sign Up Page UI

--- pages/users/signup/index.tsx
+++ pages/users/signup/index.tsx
@@ -8,12 +8,13 @@ import * as S from "./style";
 // Images
 import { SignUpImg } from "../../common/image";
 import { BackImg } from "../../common/image";
+import { HideImg } from "../../common/image";
 
 const SignUp: NextPage = () => {
     return(
         <>
         <S.Page>
-            <Link href="/">
+            <Link href="/users/login">
                 <S.Back>
                     <Image
                         src={BackImg}
@@ -23,6 +24,36 @@ const SignUp: NextPage = () => {
             <S.ContentsWrapper>
                 <S.Contents>
                     <S.Title>회원가입</S.Title>
+                    <S.UserWrapper>
+                        <S.SubTitle>닉네임</S.SubTitle>
+                        <S.Input type={"text"} placeholder="닉네임을 입력해주세요." maxLength={16} />
+                        <S.CheckBtn>확인</S.CheckBtn>
+                    </S.UserWrapper>
+                    <S.UserWrapper>
+                        <S.SubTitle>이메일</S.SubTitle>
+                        <S.Input type={"text"} placeholder="이메일을 입력해주세요." />
+                        <S.CheckBtn>확인</S.CheckBtn>
+                    </S.UserWrapper>
+                    <S.UserWrapper>
+                        <S.SubTitle>인증코드</S.SubTitle>
+                        <S.Input type={"text"} placeholder="인증번호 6자리를 입력해주세요." maxLength={6} />
+                        <S.CheckBtn>확인</S.CheckBtn>
+                    </S.UserWrapper>
+                    <S.UserWrapper>
+                        <S.SubTitle>비밀번호</S.SubTitle>
+                        <S.Input type={"password"} placeholder="비밀번호를 입력해주세요." />
+                        <S.Img>
+                            <Image src={HideImg} />
+                        </S.Img>
+                    </S.UserWrapper>
+                    <S.UserWrapper>
+                        <S.SubTitle>비밀번호 재입력</S.SubTitle>
+                        <S.Input type={"password"} placeholder="비밀번호를 재입력해주세요." />
+                        <S.Img>
+                            <Image src={HideImg} />
+                        </S.Img>
+                    </S.UserWrapper>
+                    <S.SubmitBtn>완료</S.SubmitBtn>
                 </S.Contents>
                 <S.ImageWrapper>
                     <Image src={SignUpImg} />
--- pages/users/signup/style.ts
+++ pages/users/signup/style.ts
@@ -8,6 +8,7 @@ export const Page = styled.div`
 export const Back = styled.div`
     padding-left: 100px;
     padding-top: 140px;
+    margin-right: 0px;
     width: 100px;
     height: 30px;
     :hover{
@@ -21,14 +22,80 @@ export const ContentsWrapper = styled.div`
 `;
 
 export const Contents = styled.div`
-
-`;
+    display: flex;
+    flex-direction: column;
+    margin-left: 150px;
+    `;
 
 export const Title = styled.div`
     font: 700 normal 50px ${(props) => props.theme.fonts.kr};
     color: ${(props) => props.theme.colors.Extra.black};
+    text-align: center;
+    margin-bottom: 33px;
+    `;
+
+export const UserWrapper = styled.div`
+    margin-bottom: 26px;
+`;
+
+export const SubTitle = styled.div`
+    font: 300 normal 20px ${(props) => props.theme.fonts.kr};
+    color: ${(props) => props.theme.colors.Extra.black};
+    margin-bottom: 7px;
+    margin-left: 20px;
+`;
+
+export const Input = styled.input`
+    width: 467px;
+    height: 64px;
+    border: 1px solid #929292;
+    border-radius: 10px;
+    font: 100 normal 18px ${(props) => props.theme.fonts.kr};
+    color: ${(props) => props.theme.colors.Extra.black};
+    padding-left: 20px;
+    padding-right: 150px;
+    box-sizing: border-box;
+    `;
+
+export const CheckBtn = styled.button`
+    position: relative;
+    background-color: #8ACE55;
+    border: none;
+    border-radius: 10px;
+    width: 95px;
+    height: 28px;
+    top: -2px;
+    right: 110px;
+    font: 100 normal 15px ${(props) => props.theme.fonts.kr};
+    color: ${(props) => props.theme.colors.Extra.white};
+    :hover{
+        cursor: pointer;
+    }
+`;
+
+export const Img = styled.span`
+    position: relative;
+    top: 2px;
+    right: 50px;
+    :hover{
+        cursor: pointer;
+    }
 `;
 
 export const ImageWrapper = styled.div`
+    margin-left: 120px;
+    margin-top: 50px;
+`;
 
+export const SubmitBtn = styled.button`
+    font: 500 normal 18px ${(props) => props.theme.fonts.kr};
+    color: ${(props) => props.theme.colors.Extra.white};
+    width: 467px;
+    height: 50px;
+    background-color: #FFE75F;
+    border: none;
+    :hover{
+        cursor: pointer;
+    }
+    margin-top: 38px;
 `;
